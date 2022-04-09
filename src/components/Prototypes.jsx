39be73b9525f593893e7d0c4b31c2282c64fa44e
l0u0h0 <l0u0h0@gmail.com>
2022-04-09 13:59:59 +0900

    hooks add

--- src/components/Prototypes.jsx
+++ src/components/Prototypes.jsx
@@ -1,16 +1,20 @@
-import { useContext } from "react";
-import AppStateContext from "../contexts/AppStateContext";
+import useAction from "../hooks/useAction";
+import usePrototypes from "../hooks/usePrototypes";
 
 export default function Prototypes() {
-  const { prototypes } = useContext(AppStateContext);
+  const prototypes = usePrototypes();
+  const { addToOrder } = useAction();
   return (
     <main>
       <div className="prototypes">
         {prototypes.map((prototype) => {
           const { id, thumbnail, title, price, desc, pieUrl } = prototype;
+          const click = () => {
+            addToOrder(id);
+          };
           return (
             <div className="prototype" key={id}>
-              <a href={pieUrl} target="_BLANK">
+              <a href={pieUrl} target="_BLANK" rel="noreferrer">
                 <div style={{ padding: "25px 0 33px 0," }}>
                   <video
                     muted
@@ -27,7 +31,10 @@ export default function Prototypes() {
               </a>
               <div className="prototype__body">
                 <div className="prototype__title">
-                  <div className="btn btn--primary float--right">
+                  <div
+                    className="btn btn--primary float--right"
+                    onClick={click}
+                  >
                     <i className="icon icon--plus" />
                   </div>
                   {title}
